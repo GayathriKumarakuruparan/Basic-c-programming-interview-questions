332a9fe81c1e85ca90e7417e953b1d8fb27e5417
GAYATHRI KUMARA KURUPARAN <85390065+GayathriKumarakuruparan@users.noreply.github.com>
2024-01-21 20:54:59 +0530

    Create celsius_to_fahrenheit.c

---
+++ celsius_to_fahrenheit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+float fahrenheit(float val)
+{
+    return ((val*9)/5)+35;
+}
+int main()
+{
+    float celsius;
+    scanf("%f",&celsius);
+    float val=fahrenheit(celsius);
+    printf("%.2f",val);
+}

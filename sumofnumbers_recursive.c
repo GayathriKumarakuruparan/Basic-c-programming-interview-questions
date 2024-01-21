e5efcd367b38de7e5755cbcfbe2e797521f4876a
GAYATHRI KUMARA KURUPARAN <85390065+GayathriKumarakuruparan@users.noreply.github.com>
2024-01-21 17:09:58 +0530

    Create sumofnumbers_recursive.c

---
+++ sumofnumbers_recursive.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int fact(int n)
+{
+    
+    if(n>0)
+    {
+        int fa=fact(n-1);
+        return n+fa;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    int val=fact(num);
+    printf("%d",val);
+    return 0;
+}

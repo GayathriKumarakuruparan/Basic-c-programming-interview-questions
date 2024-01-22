aec4424ece220dd503f9d956175fc23714c7ea12
GAYATHRI KUMARA KURUPARAN <85390065+GayathriKumarakuruparan@users.noreply.github.com>
2024-01-22 18:48:23 +0530

    Create remove_vowels.c

---
+++ strings/remove_vowels.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include<string.h>
+char delete_vowel(char *str)
+{
+    int i=0;
+    for(i=0;i<strlen(str);i++)
+    {
+        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' &&str[i]<= 'Z'))
+        {
+            if(((str[i]=='a')||(str[i]=='A'))||((str[i]=='e')||(str[i]=='E'))||((str[i]=='i')||(str[i]=='I'))||((str[i]=='o')||(str[i]=='O'))||((str[i]=='u')||(str[i]=='U')))
+            {
+                for(int j=i;str[j]!='\0';j++)
+                {
+                    str[j]=str[j+1];
+                }
+              i-=1;
+            }
+        }
+        
+    }
+    return *str;
+    
+}
+int main()
+{
+   char str[100];
+   printf("enter the strings :\n");
+   scanf(" %[^\n]",str);
+   delete_vowel(str);
+   printf("%s",str);
+}

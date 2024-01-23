d42ac59a9f638cd269dd0674a3536ba3fd7d0247
GAYATHRI KUMARA KURUPARAN <85390065+GayathriKumarakuruparan@users.noreply.github.com>
2024-01-23 19:59:22 +0530

    Create sorting_string_ascending.c

---
+++ strings/sorting_string_ascending.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[100];
+    printf("String before sorting - %s \n", str);
+    scanf(" %[^\n]",str);
+    for(int i=0;i<(strlen(str)-1);i++)
+    {
+        for(int j=i+1;j<strlen(str);j++)
+        {
+            if(str[i]>str[j])
+            {
+                char temp=str[i];
+                str[i]=str[j];
+                str[j]=temp;
+            }
+        }
+    }
+    printf("String before sorting - %s",str);
+}

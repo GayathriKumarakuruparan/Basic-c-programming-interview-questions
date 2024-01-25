8bd68ae16deeae94f0ce6a62ed922a83d18e2e23
GAYATHRI KUMARA KURUPARAN <85390065+GayathriKumarakuruparan@users.noreply.github.com>
2024-01-25 13:45:27 +0530

    Create 2nd_largest_number.c

---
+++ ARRAY/2nd_largest_number.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+void second_largest_number(int *arr,int *size, int *first, int *second)
+{
+    int i;
+    for(i=0;i<*size;i++)
+    {
+        if(*first<arr[i])
+        {
+            *second=*first;
+            *first=arr[i];
+        }
+        if(arr[i]<*first && *second<arr[i])
+        {
+            *second=arr[i];
+        }
+    }
+}
+int main()
+{
+    int size,first,second;
+    printf("Enter the size\n");
+    scanf("%d",&size);
+    int *arr=calloc(size,sizeof(int));
+    printf("enter the array");
+    for(int i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    first = arr[0];
+    second= arr[1];
+    second_largest_number(arr,&size,&first,&second);
+    printf("The second largest number is %d",second);
+}

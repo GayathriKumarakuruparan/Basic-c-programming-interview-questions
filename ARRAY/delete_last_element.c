73e879ba477e402a9e5b7724fc1bdd3db3d7b806
GAYATHRI KUMARA KURUPARAN <85390065+GayathriKumarakuruparan@users.noreply.github.com>
2024-01-25 21:07:05 +0530

    Create delete_last_element.c

---
+++ ARRAY/delete_last_element.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int size,new_element,position,i,last,next;
+    printf("Enter the size:\n");
+    scanf("%d",&size);
+    printf("Enter the arrays:\n");
+    int *arr=malloc(size*sizeof(int));
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Before deleting:\n");
+    for(i=0;i<size;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    size-=1;
+    arr=realloc(arr,size*sizeof(int));
+    printf("\nAfter deleting:\n");
+    for(i=0;i<size;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    free(arr);
+}

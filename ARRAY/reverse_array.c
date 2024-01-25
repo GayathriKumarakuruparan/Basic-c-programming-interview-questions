e3dd6346f59d812747f377804317d117b52bac95
GAYATHRI KUMARA KURUPARAN <85390065+GayathriKumarakuruparan@users.noreply.github.com>
2024-01-25 14:01:02 +0530

    Create reverse_array.c

---
+++ ARRAY/reverse_array.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+void swap(int *element1,int *element2)
+{
+    int temp=*element1;
+    *element1=*element2;
+    *element2=temp;
+}
+void second_largest_number(int *arr,int *size)
+{
+    int last=*size-1;
+    for(int i=0;i<(*size/2);i++)
+    {
+        swap(&arr[i],&arr[last--]);
+    }
+    for(int j=0;j<*size;j++)
+    {
+        printf("%d ",arr[j]);
+    }
+}
+int main()
+{
+    int size,first,second;
+    printf("Enter the size\n");
+    scanf("%d",&size);
+    int *arr=calloc(size,sizeof(int));
+    printf("Before reverse:\n");
+    for(int i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    first = arr[0];
+    second= arr[1];
+    printf("After reverse:\n");
+    second_largest_number(arr,&size);
+  
+}

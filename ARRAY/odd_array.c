dc7fae55a6afdf0b2aac659fd240c5a6d0cc09ad
GAYATHRI KUMARA KURUPARAN <85390065+GayathriKumarakuruparan@users.noreply.github.com>
2024-01-26 19:59:58 +0530

    Create odd_array.c

---
+++ ARRAY/odd_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+void odd_array(int *arr,int *n)
+{
+    int i;
+    for(i=0;i<*n;i++)
+    {
+        if((arr[i]%2) != 0)
+        printf("%d ",arr[i]);
+    }
+}
+int main()
+{
+    int size,i;
+    printf("Enter the size");
+    scanf("%d",&size);
+    int *arr=malloc(size*sizeof(int));
+    printf("Enter the array:\n");
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("After removed:\n");
+    odd_array(arr,&size);
+}

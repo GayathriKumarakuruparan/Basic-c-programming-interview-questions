4a20e27119f2b5606d177774cd7f975bcf467096
GAYATHRI KUMARA KURUPARAN <85390065+GayathriKumarakuruparan@users.noreply.github.com>
2024-02-03 12:16:47 +0530

    Create ascending_order_col.c

---
+++ 2DARRAY/ascending_order_col.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+void
+ascending_order_row (int col, int **arr, int row)
+{
+  int i, j;
+  int temp;
+  for (i = 0; i < row; i++)
+	{
+	  for (j = 0; j < col; j++)
+		{
+		  for (int k = (i + 1); k < col; k++)
+			{
+			  if (arr[i][j] > arr[k][j])
+				{
+				  temp = arr[i][j];
+				  arr[i][j] = arr[k][j];
+				  arr[k][j] = temp;
+				}
+			}
+		}
+	}
+}
+
+int
+main ()
+{
+  int row, col, i, j;
+  printf ("Enter the rows and columns:\n");
+  scanf ("%d %d", &row, &col);
+  int *arr[row];
+  for (i = 0; i < row; i++)
+	{
+	  arr[i] = malloc (col * sizeof (int));
+	}
+  printf ("Enter the array:\n");
+  for (i = 0; i < row; i++)
+	{
+	  for (j = 0; j < col; j++)
+		{
+		  scanf ("%d", &arr[i][j]);
+		}
+	}
+  printf ("before ordering:\n");
+  for (i = 0; i < row; i++)
+	{
+	  for (j = 0; j < col; j++)
+		{
+		  printf ("%d ", arr[i][j]);
+		}
+	  printf ("\n");
+	}
+
+  ascending_order_row (col, arr, row);
+  printf ("After ordering:\n");
+  for (i = 0; i < row; i++)
+	{
+	  for (j = 0; j < col; j++)
+		{
+		  printf ("%d ", arr[i][j]);
+		}
+	  printf ("\n");
+	}
+
+}
